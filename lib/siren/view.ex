04e6d145e049f32f844e966eed8a4fbb5c1a6c2b
Damon Kelley <damon.kelley@gmail.com>
2018-04-18 22:53:24 -0500

    Add initial support for links and sub-entities

--- lib/siren/view.ex
+++ lib/siren/view.ex
@@ -6,22 +6,37 @@ defmodule Siren.View do
     quote do
       import Siren.View
 
-      def render(_, data \\ %{}) do
+      def render(conn, data \\ %{}) do
         %{}
-        |> add_attribute("class", class())
-        |> add_attribute("properties", properties(data))
+        |> add_attribute(:class, class())
+        |> add_attribute(:properties, properties(data))
+        |> add_attribute(:entities, entities(conn, data))
+        |> add_attribute(:links, links(conn, data))
       end
 
       def class, do: nil
-      def properties(data), do: nil
+      def properties(_data), do: nil
+      def links(_conn, _data), do: nil
+      def entities(_conn, _data), do: nil
 
       defp add_attribute(data, _name, nil), do: data
-      defp add_attribute(data, name, value) do
-        Map.merge(data, %{name => value})
-      end
+      defp add_attribute(data, name, value), do: Map.merge(data, %{name => value})
 
-      defoverridable [class: 0,
-                      properties: 1]
+      defoverridable [class: 0, properties: 1, links: 2, entities: 2]
     end
+  end
+
+  def entity_link(view, conn, rel, data) do
+    %{
+      class: view.class(),
+      rel: rel,
+      href: view.self(conn, data)
+    }
+  end
+
+  def embedded(view, conn, rel, data) do
+    conn
+    |> view.render(data)
+    |> Map.put(:rel, rel)
   end
 end
--- test/siren/view_test.exs
+++ test/siren/view_test.exs
@@ -1,6 +1,7 @@
 defmodule Siren.ViewTest do
   use ExUnit.Case
   use Plug.Test
+
   defmodule TestView do
     use Siren.View
   end
@@ -11,7 +12,7 @@ defmodule Siren.ViewTest do
         conn(:get, "/")
         |> TestView.render
 
-      refute Map.get(entity, "class")
+      refute Map.get(entity, :class)
     end
 
     test "it will render the class" do
@@ -22,7 +23,7 @@ defmodule Siren.ViewTest do
       end
 
       conn = conn(:get, "/")
-      assert %{"class" => ["post"]} = PostView.render(conn)
+      assert %{:class => ["post"]} = PostView.render(conn)
     end
   end
 
@@ -32,7 +33,7 @@ defmodule Siren.ViewTest do
         conn(:get, "/")
         |> TestView.render
 
-      refute Map.get(entity, "properties")
+      refute Map.get(entity, :properties)
     end
   end
 
@@ -47,8 +48,138 @@ defmodule Siren.ViewTest do
       end
     end
 
-    assert %{"properties" => %{"Foo" => "Bar"}} =
+    assert %{:properties => %{"Foo" => "Bar"}} =
       conn(:get, "/")
       |> PostView.render(%{"foo" => "bar"})
+  end
+
+  describe "links" do
+    test "it will not render links" do
+      entity =
+        conn(:get, "/")
+        |> TestView.render
+
+      refute Map.get(entity, :links)
+    end
+
+    test "it will render a links" do
+      defmodule PostView do
+        use Siren.View
+
+        def links(_conn, %{page: page}) do
+          [
+            %{rel: :self, href: "http://example.com"},
+            %{rel: :next, href: "http://example.com?page=#{page}"},
+          ]
+        end
+      end
+
+      entity =
+        conn(:get, "/")
+        |> PostView.render(%{page: 2})
+
+      assert %{
+        :links => [
+          %{rel: :self, href: "http://example.com"},
+          %{rel: :next, href: "http://example.com?page=2"}
+        ]
+      } = entity
+    end
+  end
+
+  describe "entities" do
+    test "it will not render entities" do
+      entity =
+        conn(:get, "/")
+        |> TestView.render
+
+      refute Map.get(entity, :links)
+    end
+
+    test "it will render entities as links" do
+      defmodule CommentView do
+        use Siren.View
+
+        def class, do: [:comment]
+
+        def self(%{host: host}, %{id: id}), do: "#{host}/comments/#{id}"
+      end
+
+      defmodule PostView do
+        use Siren.View
+
+        def entities(conn, %{comment: comment}) do
+          [
+            entity_link(CommentView, conn, ["post-comment"], comment)
+          ]
+        end
+      end
+
+      entity =
+        conn(:get, "/")
+        |> Map.put(:host, "http://blog.example.com")
+        |> PostView.render(%{comment: %{id: 4}})
+
+      assert %{
+        :entities => [
+          %{
+            class: [:comment],
+            rel: ["post-comment"],
+            href: "http://blog.example.com/comments/4"
+          }
+        ]
+      } = entity
+    end
+
+    test "it will render an embeded entities" do
+      defmodule CommentView do
+        use Siren.View
+
+        def class, do: [:comment]
+
+        def self(%{host: host}, %{id: id}), do: "#{host}/comments/#{id}"
+
+        def properties(%{body: body, user: user}) do
+          %{body: body, user: user}
+        end
+
+        def links(%{host: host}, %{id: id}) do
+          [
+            %{rel: :self, href: "#{host}/comments/#{id}"},
+          ]
+        end
+      end
+
+      defmodule PostView do
+        use Siren.View
+
+        def entities(conn, %{comment: comment}) do
+          [
+            embedded(CommentView, conn, ["post-comment"], comment)
+          ]
+        end
+      end
+
+      entity =
+        conn(:get, "/")
+        |> Map.put(:host, "http://blog.example.com")
+        |> PostView.render(%{comment: %{id: 349, body: "Great post!", user: "anuuser"}})
+
+      assert %{
+        :entities => [
+          %{
+            class: [:comment],
+            rel: ["post-comment"],
+            properties: %{
+              body: "Great post!",
+              user: "anuuser"
+            },
+            links: [
+              %{rel: :self, href: "http://blog.example.com/comments/349"},
+            ]
+          }
+        ]
+      } = entity
+    end
   end
 end
